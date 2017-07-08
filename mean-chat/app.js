aa70bf43a6b4d743c2477a07439de0ffb5a7f351
Joe Kuczek <jkuczek@iastate.edu>
2017-07-08 14:26:35 -0500

    Added files for a simple MEAN angular 4 application

---
+++ mean-chat/app.js
@@ -0,0 +1,38 @@
+var express = require('express');
+var path = require('path');
+var favicon = require('serve-favicon');
+var logger = require('morgan');
+var bodyParser = require('body-parser');
+
+var chat = require('./routes/chat');
+var help = require('./routes/help');
+var app = express();
+
+app.use(logger('dev'));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({'extended':'false'}));
+app.use(express.static(path.join(__dirname, 'dist')));
+
+// add routes to our app
+app.use('/chat', chat);
+app.use('/help', help);
+
+// catch 404 and forward to error handler
+app.use(function(req, res, next) {
+  var err = new Error('Not Found');
+  err.status = 404;
+  next(err);
+});
+
+// error handler
+app.use(function(err, req, res, next) {
+  // set locals, only providing error in development
+  res.locals.message = err.message;
+  res.locals.error = req.app.get('env') === 'development' ? err : {};
+
+  // render the error page
+  res.status(err.status || 500);
+  res.render('error');
+});
+
+module.exports = app;
