b15967803157ef15cbcd8a45cc6bdcbeee087d20
Joe Kuczek <jkuczek@iastate.edu>
2017-07-09 10:25:44 -0500

    Got routes working and setup a connection to mongo

--- mean-chat/app.js
+++ mean-chat/app.js
@@ -3,6 +3,14 @@ var path = require('path');
 var favicon = require('serve-favicon');
 var logger = require('morgan');
 var bodyParser = require('body-parser');
+var mongoose = require('mongoose');
+mongoose.Promise = global.Promise;
+
+mongoose.connect('mongodb://localhost/mean-chat', {
+    useMongoClient: true
+})
+  .then(() =>  console.log('connection successful'))
+  .catch((err) => console.error(err));
 
 var chat = require('./routes/chat');
 var help = require('./routes/help');
@@ -15,7 +23,7 @@ app.use(express.static(path.join(__dirname, 'dist')));
 
 // add routes to our app
 app.use('/chat', chat);
-app.use('/help', help);
+app.use('/', help);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
@@ -30,9 +38,12 @@ app.use(function(err, req, res, next) {
   res.locals.message = err.message;
   res.locals.error = req.app.get('env') === 'development' ? err : {};
 
-  // render the error page
   res.status(err.status || 500);
-  res.render('error');
+  res.send({
+      message: err.message,
+      error: err
+  });
+  return;
 });
 
 module.exports = app;
--- mean-chat/routes/help.js
+++ mean-chat/routes/help.js
@@ -2,7 +2,7 @@ var express = require('express');
 var router = express.Router();
 
 router.get('/', function(req, res, next) {
-  res.send('API Help information');
+  res.send('API Index Page');
 });
 
 module.exports = router;
