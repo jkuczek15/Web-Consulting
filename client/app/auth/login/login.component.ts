6b9877fe00f5146648b014f8a20132d3e9d30c08
Joe Kuczek <jkuczek@iastate.edu>
2017-07-20 11:39:08 -0500

    Updated authentication to redirect to correct pages.

--- client/app/auth/login/login.component.ts
+++ client/app/auth/login/login.component.ts
@@ -1,6 +1,6 @@
-import { Component, OnInit, ViewChild, AfterViewChecked } from '@angular/core';
+import { Component, OnInit, ViewChild, AfterViewChecked, ChangeDetectorRef } from '@angular/core';
 import { NgForm, Validators, FormBuilder, FormGroup } from '@angular/forms';
-import { Router } from '@angular/router';
+import { Router, ActivatedRoute } from '@angular/router';
 import { LoginService } from './login.service';
 import { AuthService } from '../auth.service';
 import { SharedModule } from '../../includes/shared.module';
@@ -11,7 +11,7 @@ import * as UserVM from '../../includes/viewModels/User.js';
   templateUrl: './login.component.html',
   styleUrls: ['./login.component.css']
 })
-export class LoginComponent implements OnInit {
+export class LoginComponent implements OnInit, AfterViewChecked {
 
   // Grab our form reference from the template
   @ViewChild('loginForm') private currentForm: FormGroup;
@@ -22,24 +22,40 @@ export class LoginComponent implements OnInit {
   public loginForm: FormGroup;
   private user;
   private active;
+  private error;
   
   // Inject services into our constructor
   constructor(private fb: FormBuilder, 
               private router: Router,
+              private route: ActivatedRoute,
+              private cd: ChangeDetectorRef,
               private loginService: LoginService,
               private authentication: AuthService,
-              private shared: SharedModule) { }
+              private shared: SharedModule) {this.error = route.params.map(p => p.error); }
 
   ngOnInit() {
     // Create a new user VM from the User interface
     this.user = new UserVM.Login();
     this.formErrors = JSON.parse(JSON.stringify(this.user));
     this.validationMessages = this.shared.validationMessages;
-    
+
     // Create the form logic and enable the form
     this.buildForm();
     this.active = true;
   }// end onInit function
+
+  ngAfterViewChecked() {
+    this.route.params.subscribe( params => {
+      // Subscribe to route parameter changes after the DOM is ready
+      if(!params.error){
+        this.authentication.storedURL = null;
+      }// end if we need to reset the storedURL
+
+      // Set the authentication form error
+      this.formErrors['email'] = this.validationMessages[params.error];
+      this.cd.detectChanges();
+    });
+  }// end ngAfterViewChecked function (DOM READY)
 
   buildForm(): void {
     // use Regex patterns for "simple" matching
@@ -60,16 +76,17 @@ export class LoginComponent implements OnInit {
     
     // Subscribe and call this function if data in the form changes
     this.loginForm.valueChanges.subscribe(data => this.shared.onValueChanged(this, 'loginForm', data));
-    this.shared.onValueChanged(this, 'loginForm'); // set validation messages now
+    // Set validation messages now
+    this.shared.onValueChanged(this, 'loginForm'); 
   }// end buildForm function
 
   login() {
     // We have passed all client-side validation, save the user
     this.loginService.login(this.user).then((data: any) => {
-        // User is authenticated
+        // User is authenticated, store the token
         this.authentication.saveToken(data.token);
-        // Navigate to the welcome page upon success
-        this.router.navigateByUrl('/');
+        // Navigate to the stored page or the index page depending on the request
+        this.router.navigateByUrl(this.authentication.storedURL || '/');
     }, (err) => {
       if(err.status === 401){
         // User is unauthorized
