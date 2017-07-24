6f6db208624ba353ac9d1006edffe5801e5bc7c9
Joe Kuczek <jkuczek@iastate.edu>
2017-07-24 15:32:23 -0500

    Changed a few things

--- client/app/app.component.ts
+++ client/app/app.component.ts
@@ -9,7 +9,8 @@ import { AuthService } from './auth/auth.service';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent implements OnInit {
-
+  
+  // Boolean variables to keep track if we are displaying certain elements
   private show_sidebar_left: boolean = true;
   private show_sidebar_right: boolean = true;
   private show_item_spacing: boolean = false;
@@ -20,6 +21,11 @@ export class AppComponent implements OnInit {
 
   ngOnInit() {
     let self = this;
+    
+    // List of URL's to determine if we are showing/hiding certain elements
+    let no_item_spacing = ['/', '/login'];
+    let no_sidebar_right = ['/', '/play', '/login', '/register'];
+    let no_sidebar_left = ['/'];
 
     // Function to be called each time the route changes
     this.shared.onRouteChange(function() {
@@ -27,13 +33,13 @@ export class AppComponent implements OnInit {
 
       if(self.authentication.loggedIn()) {
         // user is logged in, determine when to show sidebars
-        self.show_sidebar_left = ['/'].indexOf(url) == -1;
+        self.show_sidebar_left = no_sidebar_left.indexOf(url) == -1;
       } else {
         self.show_sidebar_left = false;
       }// end if the user is logged in, show the sidebar
 
-      self.show_item_spacing = ['/', '/login'].indexOf(url) == -1;
-      self.show_sidebar_right = ['/', '/play', '/login'].indexOf(url) == -1;
+      self.show_item_spacing = no_item_spacing.indexOf(url) == -1;
+      self.show_sidebar_right = no_sidebar_right.indexOf(url) == -1;
     });
   }// end ngOninit function
 
